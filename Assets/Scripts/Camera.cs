8c17810f3c4c0b9541fc715ac2afb610385eaf9b
juanhc1498 <juanhc1498@gmail.com>
2021-05-27 18:28:42 -0500

    camara lista

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    private GameObject Obel;
-    // Start is called before the first frame update
+//variables
+    public GameObject Obel;
+    //public GameObject Evy;
+    private Vector3 PosicionCamara;
+
+    // Use this for initialization
     void Start()
     {
-        Obel = GameObject.FindWithTag("Player");
+
+        //posicion de la camara - posicion jugador para tener la posicion del jugador
+        PosicionCamara = transform.position - Obel.transform.position;
+        //PosicionCamara = transform.position - Evy.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        var pos = Obel.transform.position;
-   
-        transform.position = Obel.transform.position; 
+
+        //para cambiar la posicion actual de la camara a la del jugador
+        transform.position = Obel.transform.position + PosicionCamara;
+        //transform.position = Evy.transform.position + PosicionCamara;
     }
 }
